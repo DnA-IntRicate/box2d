118309817dd072842faf87d62dc9374064100095
Adam <87928802+DnA-IntRicate@users.noreply.github.com>
2024-01-17 14:48:48 +0200

    Premake revert to StaticLib

--- premake5.lua
+++ premake5.lua
@@ -1,50 +1,44 @@
 -- OUT_DIR and INT_DIR must be defined in the top-most premake file before including this file
 
 project "box2d"
-	language "C++"
+    language "C++"
+    kind "StaticLib"
+    warnings "Off"
 
     debugdir (OUT_DIR)
     targetdir (OUT_DIR)
     objdir (INT_DIR)
 
-	files
-	{
-		"include/**.h",
-		"src/**.h",
-		"src/**.cpp"
-	}
+    files
+    {
+        "include/**.h",
+        "src/**.h",
+        "src/**.cpp"
+    }
 
-	includedirs
-	{
-		"include",
-		"src"
-	}
+    includedirs
+    {
+        "include",
+        "src"
+    }
 
     filter "system:windows"
-		kind "SharedLib"
         systemversion "latest"
         cppdialect "C++11"
-		defines
-		{
-			"B2_SHARED",
-			"box2d_EXPORTS"
-		}
 
     filter "system:linux"
-		kind "StaticLib"
         systemversion "latest"
         cppdialect "gnu++11"
 
     filter "system:macosx"
-		kind "StaticLib"
         systemversion "latest"
         cppdialect "gnu++11"
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "Full"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "Full"
 
-	filter "configurations:Release"
-		runtime "Release"
-		symbols "Off"
-		optimize "Full"
+    filter "configurations:Release"
+        runtime "Release"
+        symbols "Off"
+        optimize "Full"
